dab1e06d4bfeb386553d5782f9485f6c857ae348
mark <markpackham1@gmail.com>
2020-11-14 22:16:38 +0000

    add deck functionality, https://youtu.be/NxRwIZWjLtE?t=1709

---
+++ script.js
@@ -0,0 +1,8 @@
+import Deck from "./deck.js";
+
+const computerCardSlot = document.querySelector(".computer-card-slot");
+
+const deck = new Deck();
+deck.shuffle();
+
+computerCardSlot.appendChild(deck.cards[0].getHTML());
